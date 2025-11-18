bdfea6ab321048d79ab6faac7e109dc6dcb0a1cb
ian tsien <boyjka.qian@outlook.com>
2025-11-19 00:25:43 +0800

    Adapt to c++ and markdown

---
+++ nvim/lua/plugins/add_plugins.lua
@@ -0,0 +1,6 @@
+return {
+  { "p00f/clangd_extensions.nvim" },
+  { "mfussenegger/nvim-dap" },
+  { "iamcco/markdown-preview.nvim" },
+  { "MeanderingProgrammer/render-markdown.nvim" },
+}
---
+++ nvim/lua/plugins/customize_plugin_specs.lua
@@ -0,0 +1,134 @@
+return {
+  -- change the sytle to night
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+    },
+  },
+
+  -- add tools to have installed below
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clang-format",
+        "prettier",
+        "markdownlint-cli2",
+        "markdown-toc",
+        "clangd",
+        "marksman",
+        "codelldb",
+      },
+    },
+  },
+
+  -- specify which formatters to use for each filetype
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        ["markdown-toc"] = {
+          condition = function(_, ctx)
+            for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+              if line:find("<!%-%- toc %-%->") then
+                return true
+              end
+            end
+          end,
+        },
+        ["markdownlint-cli2"] = {
+          condition = function(_, ctx)
+            local diag = vim.tbl_filter(function(d)
+              return d.source == "markdownlint"
+            end, vim.diagnostic.get(ctx.buf))
+            return #diag > 0
+          end,
+        },
+      },
+      formatters_by_ft = {
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+      },
+    },
+  },
+
+  -- specify which linters to use for each filetype
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        -- c = { "cpplint" },
+        -- cpp = { "cpplint" },
+        markdown = { "markdownlint-cli2" },
+      },
+    },
+  },
+
+  -- specify which lsp to use for each filetype
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          keys = {
+            { "<leader>ch", "<cmd>LspClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+          },
+          root_markers = {
+            "compile_commands.json",
+            "compile_flags.txt",
+            "configure.ac", -- AutoTools
+            "Makefile",
+            "configure.ac",
+            "configure.in",
+            "config.h.in",
+            "meson.build",
+            "meson_options.txt",
+            "build.ninja",
+            ".git",
+          },
+          capabilities = {
+            offsetEncoding = { "utf-16" },
+          },
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+          },
+        },
+        marksman = {},
+      },
+      setup = {
+        clangd = function(_, opts)
+          local clangd_ext_opts = LazyVim.opts("clangd_extensions.nvim")
+          require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+          return false
+        end,
+      },
+    },
+  },
+
+  -- specify parsers that need to be installed
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "c",
+        "cpp",
+        "markdown",
+        "markdown_inline",
+      })
+    end,
+  },
+}
